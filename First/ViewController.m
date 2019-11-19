891b738052549e30747e0deaa8f2ad401b544188
Wladislaw <wladikowalew@gmail.com>
2019-11-19 13:19:11 +0300

    First commit

--- First/ViewController.m
+++ First/ViewController.m
@@ -10,13 +10,22 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *counterL;
+@property int counter;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _counter = 0;
+    NSLog(@"begin");
+}
+
+- (IBAction)tapOnButton:(id)sender {
+    NSString *text = [NSString stringWithFormat:@"Tap: %d", (_counter++)];
+    [_counterL setText: text];
 }
 
 
