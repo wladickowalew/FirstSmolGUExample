c28666b016dd4fcf37b0ea57800b2cf7eabaf82a
Wladislaw <wladikowalew@gmail.com>
2019-11-19 10:43:05 +0300

    Initial Commit

---
+++ First/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  First
+//
+//  Created by Student on 19/11/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
